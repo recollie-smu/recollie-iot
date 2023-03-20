86cb7f9c4df3afdb4c072bd4e1c8b9e471aa4c9e
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2023-03-20 20:51:14 +0800

    update code to receive low battery notification

--- sender.py
+++ sender.py
@@ -1,10 +1,3 @@
-def on_gesture_left():
-    serial.write_string("" + ("gggg3333\n"))
-    basic.show_icon(IconNames.HAPPY)
-    basic.pause(5000)
-    basic.clear_screen()
-grove.on_gesture(GroveGesture.LEFT, on_gesture_left)
-
 def on_gesture_right():
     serial.write_string("" + ("gggg2222\n"))
     basic.show_leds("""
@@ -18,6 +11,13 @@ def on_gesture_right():
     basic.clear_screen()
 grove.on_gesture(GroveGesture.RIGHT, on_gesture_right)
 
+def on_gesture_left():
+    serial.write_string("" + ("gggg3333\n"))
+    basic.show_icon(IconNames.HAPPY)
+    basic.pause(5000)
+    basic.clear_screen()
+grove.on_gesture(GroveGesture.LEFT, on_gesture_left)
+
 def on_received_string(receivedString):
     serial.write_string(receivedString)
 radio.on_received_string(on_received_string)
@@ -29,7 +29,7 @@ serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_data_received
 radio.set_group(1)
 
 def on_forever():
-    if pins.analog_read_pin(AnalogPin.P0) == 1012:
+    if pins.analog_read_pin(AnalogPin.P0) > 1000:
         serial.write_string("" + ("gggg1111\n"))
-    basic.pause(1000)
+    basic.pause(5000)
 basic.forever(on_forever)
